55c9a42e2c36305dc0c00e5e7a26517a41d82f9c
Iván Baldo <ibaldo@netlabs.com.uy>
2024-03-01 05:02:27 +0000

    Solidity 0.8.24 consistently.

--- packages/hardhat/contracts/TokenShop.sol
+++ packages/hardhat/contracts/TokenShop.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
